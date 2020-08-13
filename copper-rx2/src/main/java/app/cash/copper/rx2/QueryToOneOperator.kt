4f747795edcb0b0c1bc8dc2030153699014b5223
Jake Wharton <jw@squareup.com>
2020-08-13 14:54:48 -0400

    Port operators to Kotlin

---
+++ copper-rx2/src/main/java/app/cash/copper/rx2/QueryToOneOperator.kt
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2017 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package app.cash.copper.rx2
+
+import android.database.Cursor
+import app.cash.copper.rx2.RxContentResolver.Query
+import io.reactivex.ObservableOperator
+import io.reactivex.Observer
+import io.reactivex.exceptions.Exceptions
+import io.reactivex.observers.DisposableObserver
+import io.reactivex.plugins.RxJavaPlugins
+
+internal class QueryToOneOperator<T>(
+  private val mapper: (Cursor) -> T,
+  /** A null `defaultValue` means nothing will be emitted when empty. */
+  private val defaultValue: T?
+) : ObservableOperator<T, Query> {
+  override fun apply(observer: Observer<in T>): Observer<in Query> {
+    return MappingObserver(observer, mapper, defaultValue)
+  }
+
+  private class MappingObserver<T>(
+    private val downstream: Observer<in T>,
+    private val mapper: (Cursor) -> T,
+    private val defaultValue: T?
+  ) : DisposableObserver<Query?>() {
+    override fun onStart() {
+      downstream.onSubscribe(this)
+    }
+
+    override fun onNext(query: Query) {
+      try {
+        var item: T? = null
+        query.run()?.use { cursor ->
+          if (cursor.moveToNext()) {
+            item = mapper(cursor)
+            if (item == null) {
+              downstream.onError(NullPointerException("QueryToOne mapper returned null"))
+              return
+            }
+            check(!cursor.moveToNext()) { "Cursor returned more than 1 row" }
+          }
+        }
+        if (!isDisposed) {
+          if (item != null) {
+            // TODO remove double-bang once on Kotlin 1.4 where 'use' has a contract.
+            downstream.onNext(item!!)
+          } else if (defaultValue != null) {
+            downstream.onNext(defaultValue)
+          }
+        }
+      } catch (e: Throwable) {
+        Exceptions.throwIfFatal(e)
+        onError(e)
+      }
+    }
+
+    override fun onComplete() {
+      if (!isDisposed) {
+        downstream.onComplete()
+      }
+    }
+
+    override fun onError(e: Throwable) {
+      if (isDisposed) {
+        RxJavaPlugins.onError(e)
+      } else {
+        downstream.onError(e)
+      }
+    }
+  }
+}
---
+++ copper-rx2/src/main/java/app/cash/copper/rx2/QueryToOptionalOperator.kt
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2017 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package app.cash.copper.rx2
+
+import android.database.Cursor
+import androidx.annotation.RequiresApi
+import app.cash.copper.rx2.RxContentResolver.Query
+import io.reactivex.ObservableOperator
+import io.reactivex.Observer
+import io.reactivex.exceptions.Exceptions
+import io.reactivex.functions.Function
+import io.reactivex.observers.DisposableObserver
+import io.reactivex.plugins.RxJavaPlugins
+import java.util.Optional
+
+@RequiresApi(24)
+internal class QueryToOptionalOperator<T>(
+  private val mapper: (Cursor) -> T
+) : ObservableOperator<Optional<T>, Query> {
+  override fun apply(observer: Observer<in Optional<T>>): Observer<in Query> {
+    return MappingObserver(observer, mapper)
+  }
+
+  private class MappingObserver<T>(
+    private val downstream: Observer<in Optional<T>>,
+    private val mapper: (Cursor) -> T
+  ) : DisposableObserver<Query>() {
+    override fun onStart() {
+      downstream.onSubscribe(this)
+    }
+
+    override fun onNext(query: Query) {
+      try {
+        var item: T? = null
+        query.run()?.use { cursor ->
+          if (cursor.moveToNext()) {
+            item = mapper(cursor)
+            if (item == null) {
+              downstream.onError(NullPointerException("QueryToOne mapper returned null"))
+              return
+            }
+            check(!cursor.moveToNext()) { "Cursor returned more than 1 row" }
+          }
+        }
+        if (!isDisposed) {
+          downstream.onNext(Optional.ofNullable(item))
+        }
+      } catch (e: Throwable) {
+        Exceptions.throwIfFatal(e)
+        onError(e)
+      }
+    }
+
+    override fun onComplete() {
+      if (!isDisposed) {
+        downstream.onComplete()
+      }
+    }
+
+    override fun onError(e: Throwable) {
+      if (isDisposed) {
+        RxJavaPlugins.onError(e)
+      } else {
+        downstream.onError(e)
+      }
+    }
+  }
+}
