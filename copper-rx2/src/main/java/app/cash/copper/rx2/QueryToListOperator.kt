265745f607c9fa510abc60c6307ea9b7b1bfd41e
Jake Wharton <jw@squareup.com>
2020-08-13 14:54:48 -0400

    Port operators to Kotlin

---
+++ copper-rx2/src/main/java/app/cash/copper/rx2/QueryToListOperator.kt
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2017 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package app.cash.copper.rx2
+
+import android.database.Cursor
+import app.cash.copper.rx2.RxContentResolver.Query
+import io.reactivex.ObservableOperator
+import io.reactivex.Observer
+import io.reactivex.exceptions.Exceptions
+import io.reactivex.functions.Function
+import io.reactivex.observers.DisposableObserver
+import io.reactivex.plugins.RxJavaPlugins
+import java.util.ArrayList
+
+internal class QueryToListOperator<T>(
+  private val mapper: (Cursor) -> T
+) : ObservableOperator<List<T>, Query> {
+  override fun apply(observer: Observer<in List<T>>): Observer<in Query> {
+    return MappingObserver(observer, mapper)
+  }
+
+  private class MappingObserver<T>(
+    private val downstream: Observer<in List<T>>,
+    private val mapper: (Cursor) -> T
+  ) : DisposableObserver<Query>() {
+    override fun onStart() {
+      downstream.onSubscribe(this)
+    }
+
+    override fun onNext(query: Query) {
+      try {
+        val cursor = query.run()
+        if (cursor == null || isDisposed) {
+          return
+        }
+        val items = ArrayList<T>(cursor.count)
+        cursor.use {
+          while (cursor.moveToNext()) {
+            items.add(mapper(cursor))
+          }
+        }
+        if (!isDisposed) {
+          downstream.onNext(items)
+        }
+      } catch (e: Throwable) {
+        Exceptions.throwIfFatal(e)
+        onError(e)
+      }
+    }
+
+    override fun onComplete() {
+      if (!isDisposed) {
+        downstream.onComplete()
+      }
+    }
+
+    override fun onError(e: Throwable) {
+      if (isDisposed) {
+        RxJavaPlugins.onError(e)
+      } else {
+        downstream.onError(e)
+      }
+    }
+  }
+}
